f89f0439d056ca79e23ace1f3abc89c386a07a86
AadiCool <silverswordaadi05@gmail.com>
2020-03-07 14:50:49 +0530

    Performed Left Shift

--- Assignment1/isim/muxtest_isim_beh.exe.sim/work/m_00000000002940720495_1152419271.c
+++ Assignment1/isim/muxtest_isim_beh.exe.sim/work/m_00000000002940720495_1152419271.c
@@ -21,8 +21,9 @@
 #include <malloc.h>
 #define alloca _alloca
 #endif
-static const char *ng0 = "D:/VLSI/Assignment1/muxtest.v";
+static const char *ng0 = "D:/studies/6th sem/VLSI lab/DigiVLSI/Digital-VLSI/Assignment1/muxtest.v";
 static int ng1[] = {0, 0};
+static int ng2[] = {1, 0};
 
 
 
@@ -59,7 +60,151 @@ LAB4:    xsi_set_current_line(45, ng0);
     *((char **)t1) = &&LAB5;
 
 LAB1:    return;
-LAB5:    goto LAB1;
+LAB5:    xsi_set_current_line(53, ng0);
+    t2 = ((char*)((ng1)));
+    t3 = (t0 + 1448);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(53, ng0);
+    t2 = ((char*)((ng1)));
+    t3 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(53, ng0);
+    t2 = ((char*)((ng1)));
+    t3 = (t0 + 1768);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(54, ng0);
+    t2 = (t0 + 2496);
+    xsi_process_wait(t2, 100000LL);
+    *((char **)t1) = &&LAB6;
+    goto LAB1;
+
+LAB6:    xsi_set_current_line(55, ng0);
+    t2 = ((char*)((ng1)));
+    t3 = (t0 + 1448);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(55, ng0);
+    t2 = ((char*)((ng1)));
+    t3 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(55, ng0);
+    t2 = ((char*)((ng2)));
+    t3 = (t0 + 1768);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(56, ng0);
+    t2 = (t0 + 2496);
+    xsi_process_wait(t2, 100000LL);
+    *((char **)t1) = &&LAB7;
+    goto LAB1;
+
+LAB7:    xsi_set_current_line(57, ng0);
+    t2 = ((char*)((ng1)));
+    t3 = (t0 + 1448);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(57, ng0);
+    t2 = ((char*)((ng2)));
+    t3 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(57, ng0);
+    t2 = ((char*)((ng1)));
+    t3 = (t0 + 1768);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(58, ng0);
+    t2 = (t0 + 2496);
+    xsi_process_wait(t2, 100000LL);
+    *((char **)t1) = &&LAB8;
+    goto LAB1;
+
+LAB8:    xsi_set_current_line(59, ng0);
+    t2 = ((char*)((ng1)));
+    t3 = (t0 + 1448);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(59, ng0);
+    t2 = ((char*)((ng2)));
+    t3 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(59, ng0);
+    t2 = ((char*)((ng2)));
+    t3 = (t0 + 1768);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(60, ng0);
+    t2 = (t0 + 2496);
+    xsi_process_wait(t2, 100000LL);
+    *((char **)t1) = &&LAB9;
+    goto LAB1;
+
+LAB9:    xsi_set_current_line(61, ng0);
+    t2 = ((char*)((ng2)));
+    t3 = (t0 + 1448);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(61, ng0);
+    t2 = ((char*)((ng1)));
+    t3 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(61, ng0);
+    t2 = ((char*)((ng1)));
+    t3 = (t0 + 1768);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(62, ng0);
+    t2 = (t0 + 2496);
+    xsi_process_wait(t2, 100000LL);
+    *((char **)t1) = &&LAB10;
+    goto LAB1;
+
+LAB10:    xsi_set_current_line(63, ng0);
+    t2 = ((char*)((ng2)));
+    t3 = (t0 + 1448);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(63, ng0);
+    t2 = ((char*)((ng1)));
+    t3 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(63, ng0);
+    t2 = ((char*)((ng2)));
+    t3 = (t0 + 1768);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(64, ng0);
+    t2 = (t0 + 2496);
+    xsi_process_wait(t2, 100000LL);
+    *((char **)t1) = &&LAB11;
+    goto LAB1;
+
+LAB11:    xsi_set_current_line(65, ng0);
+    t2 = ((char*)((ng2)));
+    t3 = (t0 + 1448);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(65, ng0);
+    t2 = ((char*)((ng2)));
+    t3 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(65, ng0);
+    t2 = ((char*)((ng1)));
+    t3 = (t0 + 1768);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(66, ng0);
+    t2 = (t0 + 2496);
+    xsi_process_wait(t2, 100000LL);
+    *((char **)t1) = &&LAB12;
+    goto LAB1;
+
+LAB12:    xsi_set_current_line(67, ng0);
+    t2 = ((char*)((ng2)));
+    t3 = (t0 + 1448);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(67, ng0);
+    t2 = ((char*)((ng2)));
+    t3 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(67, ng0);
+    t2 = ((char*)((ng2)));
+    t3 = (t0 + 1768);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+    xsi_set_current_line(68, ng0);
+    t2 = (t0 + 2496);
+    xsi_process_wait(t2, 100000LL);
+    *((char **)t1) = &&LAB13;
+    goto LAB1;
+
+LAB13:    goto LAB1;
 
 }
 
--- Assignment1/isim/muxtest_isim_beh.exe.sim/work/m_00000000003430474034_0132817699.c
+++ Assignment1/isim/muxtest_isim_beh.exe.sim/work/m_00000000003430474034_0132817699.c
@@ -21,7 +21,7 @@
 #include <malloc.h>
 #define alloca _alloca
 #endif
-static const char *ng0 = "D:/VLSI/Assignment1/mux2to1.v";
+static const char *ng0 = "D:/studies/6th sem/VLSI lab/DigiVLSI/Digital-VLSI/Assignment1/mux2to1.v";
 
 
 
